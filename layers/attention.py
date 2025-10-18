fix type hints, kv cache bugs, attention mask, etc.

layers/attention.py
@@ -22,7 +22,7 @@ class Attention(nn.Module):
         self.key_dim = key_dim
         self.num_heads = num_heads
         self.eps = eps
-        self.rms_norm = RMSNorm(hidden_dim, eps=1e-5)
+        self.rms_norm = RMSNorm(hidden_dim, eps)
         self.rope_theta = rope_theta
         self.q_proj = nn.Linear(hidden_dim,key_dim)
         self.k_proj = nn.Linear(hidden_dim,key_dim)
@@ -48,13 +48,16 @@ class Attention(nn.Module):
             sin * x1 + cos * x2
         )
 
-    def forward(self,x: torch.Tensor, key_cache: Optional[Tensor] = None, value_cache: Optional[Tensor] = None,) -> torch.Tensor: 
+    def forward(self,x: torch.Tensor, key_cache: Optional[Tensor] = None, value_cache: Optional[Tensor] = None,) -> tuple[torch.Tensor, Optional[tuple[Tensor, Tensor]]]: 
         residual = x  # b s h
         x_norm = self.rms_norm(x)
-        q = self.q_proj(x_norm)
+        if self.use_cache:
+            q = self.q_proj(x_norm[:,-1:,:])
+        else:
+            q = self.q_proj(x_norm)
         if self.use_cache and key_cache is not None:
             k = key_cache
-            k_new = self.k_proj(x[:,-1:,:])
+            k_new = self.k_proj(x_norm[:,-1:,:])
             k = torch.cat((k,k_new),dim=1)
         else:
             k = self.k_proj(x_norm)
@@ -64,7 +67,7 @@ class Attention(nn.Module):
             key_cache = k
         if self.use_cache and value_cache is not None:
             v = value_cache
-            v_new = self.v_proj(x[:,-1:,:])
+            v_new = self.v_proj(x_norm[:,-1:,:])
             v = torch.cat((v,v_new),dim=1)
         else:
             v = self.v_proj(x_norm)
@@ -81,9 +84,10 @@ class Attention(nn.Module):
         k = self.apply_rope(k)
         
         attention_scores = torch.einsum("bshd,bthd->bhst", q, k) / math.sqrt(self.key_dim // self.num_heads) #b h s t
-        S = residual.size(1)
+        S = q.size(1)
+        T = x.size(1)
         attention_mask = torch.tril(
-            torch.ones(S, S, dtype=torch.bool, device=x.device)
+            torch.ones(S, T, dtype=torch.bool, device=x.device)
         ).unsqueeze(0) # 1 s t
         attention_scores = attention_scores.masked_fill(~attention_mask, float("-inf"))
         softmax_scores = torch.softmax(attention_scores, dim=-1)

layers/positional_embedding.py
@@ -9,5 +9,5 @@ class PositionalEmbedding(nn.Module):
         self.max_length = max_length
         self.embedding = nn.Embedding(max_length, hidden_dim)
 
-    def forward(self, x: int) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.embedding(x)

moe_transformer.py
@@ -37,7 +37,7 @@ class Transformer(nn.Module):
         self.layers = nn.ModuleList()
         self.use_cache = use_cache
         for i in range(num_layers):
-            self.layers.append(Attention(hidden_dim, key_dim, num_heads, eps, use_cache=use_cache))
+            self.layers.append(Attention(hidden_dim, key_dim, num_heads, use_cache=use_cache))
             self.layers.append(SwiGLU(hidden_dim, intermediate_dim))
             if i in moe_layers:
                 self.layers.append(

transformer.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 
-from layers.positional_embedding import PositionalEmbedding
 from layers.swiglu import SwiGLU
 from layers.attention import Attention
 from layers.rmsnorm import RMSNorm
@@ -16,15 +15,13 @@ class Transformer(nn.Module):
         key_dim: int,
         num_heads: int,
         intermediate_dim: int,
-        max_length: int,
         eps: float = 1e-6,
     ):
 
         super().__init__()
         self.num_layers = num_layers
-        self.RMSNorm = RMSNorm(eps)
+        self.RMSNorm = RMSNorm(hidden_dim,eps)
         self.embedding = nn.Embedding(vocab_size, hidden_dim)
-        self.positional_embedding = PositionalEmbedding(hidden_dim, max_length)
         self.unembedding = nn.Linear(hidden_dim, vocab_size, bias=False)
         layers = []
         for _ in range(num_layers):
@@ -33,7 +30,7 @@ class Transformer(nn.Module):
         self.model = nn.Sequential(*layers)
 
     def forward(self, x: int) -> torch.Tensor:
-        x = self.embedding(x) + self.positional_embedding(x)
+        x = self.embedding(x)
         x = self.model(x)
         x = self.unembedding(x)
         return x

utils/generator.py
@@ -2,12 +2,7 @@ import torch
 from transformers import AutoTokenizer
 
 
-
-tokenizer = AutoTokenizer.from_pretrained(
-    "fla-hub/gla-1.3B-100B", trust_remote_code=True
-)
-
-def generate(model, model_config, prompt, device, use_cache, max_length: int = 100, temperature: float = 1.0) -> str:
+def generate(model, tokenizer, model_config, prompt, device, use_cache, max_length: int = 100, temperature: float = 1.0) -> str:
     inputs = tokenizer(prompt, return_tensors="pt")["input_ids"].to(device)
     if use_cache:
         key_cache = [None for _ in range(model_config["num_layers"])]
@@ -15,7 +10,7 @@ def generate(model, model_config, prompt, device, use_cache, max_length: int = 1
     with torch.no_grad():
         for _ in range(max_length):
             if use_cache:
-                outputs, (key_cache, value_cache) = model(inputs,key_cache,value_cache)
+                outputs, (key_cache, value_cache) = model(inputs[-1].unsqueeze(0),key_cache,value_cache)
             else: 
                 outputs, _ = model(inputs)
             next_token_logits = outputs[:, -1, :]